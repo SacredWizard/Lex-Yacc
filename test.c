ad3071137c592fd732b94aa4539073b193610ba0
Rakesh R <rakesh2693@gmail.com>
2014-07-11 00:33:46 +0530

    First Commit

---
+++ test.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int x,z[50];
+	float y;
+    char *ptr;
+    *ptr = 0;
+	scanf("%f",&y);
+	x=1;
+    x++;
+	printf(" ew ");
+ 	printf("qqq %f www ",y);
+	
+   	
+    if(x<y)
+	{
+		x=y;
+	}
+	else
+    a--;
+    for(i=0;i<d;i++)
+    {
+   		for(j=0;j<n;j++)
+		{
+			x=y-100;
+			y+=98;
+			x--;
+			++y;
+		}
+    }
+	
+	return 0;
+}
